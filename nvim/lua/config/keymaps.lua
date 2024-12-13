5ccad39258a173a8039d4c64815e50418cc2fd63
Lameck Santos <lameck2@hotmail.com>
2024-12-13 20:42:36 -0300

    feat(-nvim): Add nvim files config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- local builtin = require("telescope.builtin")
+-- vim.keymap.set("n", "<leader>ps", function()
+--   builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
