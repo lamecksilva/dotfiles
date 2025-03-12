86f3ed8c3825a6b43654b54637a6c58599f169c5
Lameck Santos <lameck2@hotmail.com>
2025-03-12 12:47:31 -0300

    feat(-color): Add transparency to colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { style = "night" },
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
 }
