1475aa77b3dc5a6c43fd66b99ef1789c3578f217
Lameck Santos <lameck2@hotmail.com>
2025-04-07 17:34:01 -0300

    feat(-lazy): Remove example (default) file, add blink config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,7 +17,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "gruvbox",
+    } },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -30,10 +32,10 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight" } },
+  install = {},
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
+    notify = true, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -11,4 +11,6 @@ return {
       },
     },
   },
+
+  { "ellisonleao/gruvbox.nvim" },
 }
