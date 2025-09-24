10c282a356109557bff93d85461c1532c9cebc94
Lameck Santos <lameck2@hotmail.com>
2025-09-24 10:29:56 -0300

    fix: use prettier to format ts, js and json files

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,28 +1,9 @@
 return {
-  -- Temas escuros populares
-  -- { "folke/tokyonight.nvim" },
-  { "catppuccin/nvim", name = "catppuccin" },
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- { "rebelot/kanagawa.nvim" },
-  -- { "EdenEast/nightfox.nvim" },
-  -- { "rose-pine/neovim", name = "rose-pine" },
-
-  -- { "projekt0n/github-nvim-theme" },
-  -- { "sainnhe/everforest" },
-
-  -- Configurar tema ativo (descomente o que quiser usar)
+  { "rebelot/kanagawa.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "tokyonight-night",
-      colorscheme = "catppuccin-mocha",
-      -- colorscheme = "gruvbox",
-      -- colorscheme = "kanagawa-wave",
-      -- colorscheme = "nightfox",
-      -- colorscheme = "rose-pine",
-      -- colorscheme = "github_dark",
-      -- colorscheme = "everforest",
-      -- colorscheme = "everforest",
+      colorscheme = "kanagawa-wave",
     },
   },
 }
