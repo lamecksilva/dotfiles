b06290a74a62d1c8f9af0494943f039ee966e522
Lameck Santos <lameck2@hotmail.com>
2026-04-30 17:54:57 -0300

    feat: add dap to javascript/typescript vscode-js-debug

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -2,37 +2,68 @@ return {
   "mfussenegger/nvim-dap",
   opts = function()
     local dap = require("dap")
-    
-    -- Node.js adapter
-    dap.adapters.node2 = {
-      type = "executable",
-      command = "node",
-      args = { vim.fn.stdpath("data") .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
+
+    -- -- Node.js adapter
+    -- dap.adapters.node2 = {
+    --   type = "executable",
+    --   command = "node",
+    --   args = { vim.fn.stdpath("data") .. "/mason/packages/node-debug2-adapter/out/src/nodeDebug.js" },
+    -- }
+    --
+    -- -- NestJS configuration
+    -- dap.configurations.typescript = {
+    --   {
+    --     name = "Launch NestJS",
+    --     type = "node2",
+    --     request = "launch",
+    --     program = "${workspaceFolder}/dist/main.js",
+    --     cwd = "${workspaceFolder}",
+    --     sourceMaps = true,
+    --     protocol = "inspector",
+    --     console = "integratedTerminal",
+    --   },
+    --   {
+    --     name = "Attach to NestJS",
+    --     type = "node2",
+    --     request = "attach",
+    --     processId = require("dap.utils").pick_process,
+    --     cwd = "${workspaceFolder}",
+    --     sourceMaps = true,
+    --     protocol = "inspector",
+    --   },
+    -- }
+    --
+    -- dap.configurations.javascript = dap.configurations.typescript
+
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        -- 💀 Make sure to update this path to point to your installation
+        args = { "~/js-debug/src/dapDebugServer.js", "${port}" },
+      },
     }
-    
-    -- NestJS configuration
-    dap.configurations.typescript = {
+
+    dap.configurations.javascript = {
       {
-        name = "Launch NestJS",
-        type = "node2",
+        type = "pwa-node",
         request = "launch",
-        program = "${workspaceFolder}/dist/main.js",
+        name = "Launch file",
+        program = "${file}",
         cwd = "${workspaceFolder}",
-        sourceMaps = true,
-        protocol = "inspector",
-        console = "integratedTerminal",
       },
       {
-        name = "Attach to NestJS",
-        type = "node2",
+
+        type = "pwa-node",
         request = "attach",
+        name = "Attatch",
         processId = require("dap.utils").pick_process,
         cwd = "${workspaceFolder}",
-        sourceMaps = true,
-        protocol = "inspector",
       },
     }
-    
-    dap.configurations.javascript = dap.configurations.typescript
+
+    dap.configurations.typescript = dap.configurations.javascript
   end,
 }
