1de4db1634965381c186c8d9719bdedbaecaf9a0
Lameck Santos <lameck2@hotmail.com>
2026-04-30 10:25:50 -0300

    feat: add COQ to snippets

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,11 +1,34 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+      -- main one
+      { "ms-jpq/coq_nvim", branch = "coq" },
+
+      -- 9000+ Snippets
+      { "ms-jpq/coq.artifacts", branch = "artifacts" },
+
+      -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+      -- Need to **configure separately**
+      { "ms-jpq/coq.thirdparty", branch = "3p" },
+    },
+    init = function()
+      vim.g.coq_settings = {
+        auto_start = true,
+      }
+    end,
     opts = {
       servers = {
         eslint = {
           root_dir = function(fname)
-            return require("lspconfig.util").root_pattern(".eslintrc", ".eslintrc.js", ".eslintrc.json", "eslint.config.js", "package.json")(fname)
+            return require("lspconfig.util").root_pattern(
+              ".eslintrc",
+              ".eslintrc.js",
+              ".eslintrc.json",
+              "eslint.config.js",
+              "package.json"
+            )(fname)
           end,
         },
         pyright = {
@@ -31,4 +54,12 @@ return {
       },
     },
   },
+  -- {
+  --   "mason-org/mason-lspconfig.nvim",
+  --   opts = {},
+  --   dependencies = {
+  --     { "mason-org/mason.nvim", opts = {} },
+  --     "neovim/nvim-lspconfig",
+  --   },
+  -- },
 }
